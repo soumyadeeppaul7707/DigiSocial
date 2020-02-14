3acea0c9ab29b6962857c614b331e86f6534760b
soumyadeeppaul7707 <soumyadeeppaul7707@gmail.com>
2020-02-15 03:44:18 +0530

    Profile image structure UI done.

--- digisocialUI/src/app/modules/body/profile/details/details.component.ts
+++ digisocialUI/src/app/modules/body/profile/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -10,6 +11,46 @@ export class DetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeListener($event): void {
+    this.readThis($event.target);
+    // imagecropModal.setTimeout(() => {
+
+    // }, 3000);
+  }
+
+  image:any='assets/1.jpg';
+  file: File = null;
+
+  readThis(inputValue: any): void {
+    this.file = inputValue.files[0];
+    var myReader: FileReader = new FileReader();
+
+    myReader.onloadend = (e) => {
+      this.image = myReader.result;
+    }
+    myReader.readAsDataURL(this.file);
+
+    
+
+  }
+
+  upload() {
+
+    
+    // let jsonData = ({
+    //   "blobobject": this.image,
+    //   "name": this.file.name
+    // });
+
+    // console.log(jsonData);
+
+    // this.uiService.fileUpload(jsonData).subscribe(
+    //   data => {
+    //     console.log("Yayy ! done mannnn !!!!!!!!!!!!!");
+    //   }
+    // );
   }
 
 }
